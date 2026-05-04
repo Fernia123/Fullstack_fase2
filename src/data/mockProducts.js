6baaa5d67f8ba2a255d79e76cd0150b86967d651
AndhreSoriano <daitiut@gmail.com>
2026-05-04 16:10:40 -0500

    Delete src directory

--- src/data/mockProducts.js
+++
@@ -1,4 +0,0 @@
-export const mockProducts = [
-    {id: '1', nombre: 'MacBook M2', categoria: 'Laptop', precio: 1299, stock: 5},
-    {id: '2', nombre: 'HP', categoria: 'Laptop', precio: 5000, stock: 2},
-];
